76076f58f400701ca17414b9bcd54b3090a59182
Khalil Al-Mujeeb <khalilalm@users.noreply.github.com>
2020-07-15 17:12:23 -0400

    make it better

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const express = require('express');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
-dotenv.config({ path: './config.env' })
-app.use(cors())
+dotenv.config({ path: "./config.env" });
+app.use(cors());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Profile and Rank routes
-app.use('/api/v1/profile', require('./routes/profile'));
-app.use('/api/v1/rank', require('./routes/rank'));
+app.use("/api/v1/profile", require("./routes/profile"));
+app.use("/api/v1/rank", require("./routes/rank"));
 // Handle production
-if (process.env.NODE_ENV ==="production"){
-  app.use(express.static('apexer/build'))
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'apexer', 'build', 'index.html'))
-
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("apexer/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "apexer", "build", "index.html"));
+  });
 }
 
 const port = process.env.PORT || 5000;
