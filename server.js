e0754bf238892fdaeb8506545fa9e6ebd6aa0610
Khalil Al-Mujeeb <khalilalm@users.noreply.github.com>
2020-07-15 00:23:29 -0400

    make it better

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/v1/rank', require('./routes/rank'));
 if (process.env.NODE_ENV ==="production"){
   app.use(express.static('apexer/build'))
   app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'apexer', 'build', index.html))
+    res.sendFile(path.resolve(__dirname, 'apexer', 'build', 'index.html'))
 
   })
 }
