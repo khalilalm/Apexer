9ab13037580468f0b759ee604ba2b1fc821043ee
Khalil Al-Mujeeb <khalilalm@users.noreply.github.com>
2022-10-30 14:28:34 -0400

    added video section

--- apexer/src/Videos_Guides/vg.js
+++ apexer/src/Videos_Guides/vg.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class vg extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <div class="p-8 ">
-                
-                <h1 class="bg-red-700 border-red-600 border-b p-4 m-4 rounded"> Videos & Guides</h1>
-                <div class="p-8 " >
-                    <img class="w-50 h-100 rounded-md" src="https://placeimg.com/840/480/any" />
-                </div>
-                <div src="https://placeimg.com/840/480/any"></div>
-            </div>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <div class="p-8 ">
+        <h1 class="bg-red-700 border-red-600 border-b p-4 m-4 rounded">
+          {" "}
+          Videos & Guides
+        </h1>
+        <div class="p-8 ">
+          <img
+            class="w-50 h-100 rounded-md"
+            src="https://placeimg.com/840/480/any"
+          />
+        </div>
+        <div class="flex">
+          <img src="https://placeimg.com/840/480/any" />
+          <img src="https://placeimg.com/840/480/any" />
+          <img src="https://placeimg.com/840/480/any" />
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default vg;
